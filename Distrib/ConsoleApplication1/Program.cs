ebdd2eb00b87e5801c372ca374b34f05e480d3dc
cpearson1990_cp <SND\cpearson1990_cp>
2012-10-12 20:26:56 +0000

    Initial workings on assembly loading mechanism.

--- Distrib/ConsoleApplication1/Program.cs
+++ Distrib/ConsoleApplication1/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Distrib.Processes;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.ComponentModel.Composition.Hosting;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -17,20 +17,94 @@ namespace ConsoleApplication1
             var p = new Program();
             p.Run();
         }
-
-        [ImportMany]
-        private Lazy<IDistribProcess, IDistribProcessMetadata>[] Processes { get; set; }
         
         public void Run()
         {
-            var catalog = new AggregateCatalog();
-            catalog.Catalogs.Add(new AssemblyCatalog(typeof(IDistribProcess).Assembly));
-            var container = new CompositionContainer(catalog);
-            container.ComposeParts(this);
+            var dir = @"C:\Users\Clint\Desktop\distrib plugins\";
 
-            if (Processes != null && Processes.Length > 0)
+            foreach (var plugindll in Directory.EnumerateFiles(dir, "*.dll"))
             {
-                
+                var ad = AppDomain.CreateDomain(plugindll);
+                var asmManager = ad.CreateInstanceAndUnwrap(typeof(AppDomainAssemblyManager).Assembly.FullName,
+                    typeof(AppDomainAssemblyManager).FullName) as AppDomainAssemblyManager;
+                asmManager.LoadAssembly(plugindll);
+                IDistribProcess proc = asmManager.CreateProcessTypeFromAssembly(plugindll,
+                    "TestLibrary.TestDistribProcess");
+                var s = proc.SayHello();
+                AppDomain.Unload(ad);
+                File.Delete(plugindll);
+            }
+        }
+    }
+
+    public sealed class IDistribProcessProxy : MarshalByRefObject, IDistribProcess
+    {
+        private IDistribProcess m_masked = null;
+
+        public IDistribProcessProxy(IDistribProcess maskedProcess)
+        {
+            m_masked = maskedProcess;
+        }
+
+        public string SayHello()
+        {
+            return m_masked.SayHello();
+        }
+    }
+
+    public sealed class AppDomainAssemblyManager : MarshalByRefObject
+    {
+        private ConcurrentDictionary<string, Assembly> m_dictAssemblies =
+            new ConcurrentDictionary<string, Assembly>();
+
+        public void LoadAssembly(string filePath)
+        {
+            try
+            {
+                var asm = Assembly.LoadFile(filePath);
+                if (m_dictAssemblies.TryAdd(filePath, asm) == false)
+                {
+                    throw new ApplicationException("Failed to add assembly to store");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Failed to load assembly", ex);
+            }
+        }
+
+        public IDistribProcess CreateProcessTypeFromAssembly(string filePath, string typeName)
+        {
+            try
+            {
+                Assembly asm = null;
+                if (m_dictAssemblies.TryGetValue(filePath, out asm) == false)
+                {
+                    throw new ApplicationException("Failed to find assembly in store");
+                }
+
+                var obj = asm.CreateInstance(typeName) as IDistribProcess;
+                return new IDistribProcessProxy(obj);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Failed to get process type from assembly", ex);
+            }
+        }
+
+        public void RemoveAssembly(string filePath)
+        {
+            try
+            {
+                Assembly asm = null;
+                if (m_dictAssemblies.TryRemove(filePath, out asm) == false)
+                {
+                    throw new ApplicationException("Failed to remove assembly from store");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Failed to remove assembly", ex);
             }
         }
     }
---
+++ Distrib/TestLibrary/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Distrib.Processes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    public sealed class TestDistribProcess : IDistribProcess
+    {
+
+        public string SayHello()
+        {
+            return "Howdy!";
+        }
+    }
+}
