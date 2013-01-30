a1977eaad83bf8eb2652b2b0c1daab95987a3bf7
cpearson1990_cp <SND\cpearson1990_cp>
2013-01-30 22:42:19 +0000

    Main region now registered and content being displayed

---
+++ Distrib/ProcessRunner/Modules/ProcessRunnerMainModule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Prism.MefExtensions.Modularity;
+using Microsoft.Practices.Prism.Modularity;
+using Microsoft.Practices.Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessRunner.Modules
+{
+    [ModuleExport(typeof(ProcessRunnerMainModule))]
+    public class ProcessRunnerMainModule : IModule
+    {
+        private IRegionManager _regionManager;
+
+        [ImportingConstructor()]
+        public ProcessRunnerMainModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.ProcessRunnerMainView));
+        }
+    }
+}
---
+++ Distrib/ProcessRunner/ViewModels/ProcessRunnerMainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Prism.Events;
+using ProcessRunner.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessRunner.ViewModels
+{
+    [Export]
+    [PartCreationPolicy(System.ComponentModel.Composition.CreationPolicy.NonShared)]
+    public sealed class ProcessRunnerMainViewModel : ViewModelBase
+    {
+        private IAppStateService _appState;
+        private IDistribInteractionService _distribService;
+        private IEventAggregator _eventAgg;
+
+        [ImportingConstructor]
+        public ProcessRunnerMainViewModel(Services.IDistribInteractionService distribService,
+            IEventAggregator eventAggregator, Services.IAppStateService appState)
+        {
+            _appState = appState;
+            _distribService = distribService;
+            _eventAgg = eventAggregator;
+        }
+    }
+}
